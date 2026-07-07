1dee5fd3d0adf684d9d2f65f03a969c13ba65b27
Jay <jacksongardiner311@gmail.com>
2026-07-07 14:36:20 +1000

    Added Windows compatability (Issue fixed)

--- RobloxLauncher/Main.java
+++ RobloxLauncher/Main.java
@@ -3,17 +3,14 @@ import javax.sound.sampled.*;
 import java.io.IOException;
 import java.io.File;
 import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("ROADBLOCKS LAUNCHER (SAFE)");
         window.setSize(500, 400);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLayout(null);
-
         ImageIcon icon = new ImageIcon("RobloxLauncher/images/icon.png");
         window.setIconImage(icon.getImage());
-
         ImageIcon backgroundImage = new ImageIcon("RobloxLauncher/images/background.png");
         JLabel background = new JLabel(backgroundImage);
         background.setBounds(0, 0, 500, 400);
@@ -25,17 +22,23 @@ public class Main {
             launchButton.setText("YAYYY!");
             new javax.swing.Timer(3000, evt -> launchButton.setText("LAUNCH ROADBLOCKS!!!")).start();
             try {
-                ProcessBuilder pb = new ProcessBuilder("open", "-a", "Roblox");
-                pb.start();
+                String os = System.getProperty("os.name").toLowerCase();
+                if (os.contains("mac")) {
+                    new ProcessBuilder("open", "-a", "Roblox").start();
+                } else if (os.contains("win")) {
+                    new ProcessBuilder("cmd", "/c", "start", "", "Roblox").start();
+                } else if (os.contains("nux") || os.contains("nix")) {
+                    new ProcessBuilder("roblox").start();
+                } else {
+                    System.out.println("Unsupported OS: " + os);
+                }
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         });
-
         window.add(launchButton);
         window.add(background);
         window.setVisible(true);
-
 try {
     File musicFile = new File("RobloxLauncher/audio/music.wav");
     AudioInputStream audioStream = AudioSystem.getAudioInputStream(musicFile);
@@ -45,6 +48,5 @@ try {
 } catch (Exception e) {
     e.printStackTrace();
 }
-
     }
 }
